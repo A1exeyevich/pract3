2ea9910484af4d92e88559be24e947a22faf6afd
Хоменко Владимир <127117566+A1exeyevich@users.noreply.github.com>
2024-11-22 11:34:31 +0900

    pract

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using USQLCSharpProject1;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyMath math = new MyMath();
+          
+            math.PrintHello();
+            math.PrintSum(3, 5);
+
+            int sum = math.Add(10, 20);
+            Console.WriteLine($"Сумма = {sum}");
+
+            string greeting = math.GetGreeting("Владимир");
+            Console.WriteLine(greeting);
+
+            double area = math.CalculateCircleArea(5);
+            Console.WriteLine($"Площадь круга = {area}");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ USQLCSharpProject1/Class1.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Analytics.Interfaces;
+using Microsoft.Analytics.Interfaces.Streaming;
+using Microsoft.Analytics.Types.Sql;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace USQLCSharpProject1
+{
+    public class MyMath
+    {
+        // Метод с типом void
+        public void PrintHello()
+        {
+            Console.WriteLine("Hello from DLL!");
+        }
+
+        // Метод с типом void
+        public void PrintSum(int a, int b)
+        {
+            Console.WriteLine($"Сумма {a} и {b} = {a + b}");
+        }
+
+        // Метод с возвращаемым значением int
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        // Метод с возвращаемым значением string
+        public string GetGreeting(string name)
+        {
+            return $"Здравствуйте, {name}!";
+        }
+
+        // Метод с возвращаемым значением double
+        public double CalculateCircleArea(double radius)
+        {
+            return Math.PI * radius * radius;
+        }
+    }
+}
